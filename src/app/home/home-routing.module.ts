caa87158aba9262d292536bd67099285f8c0a666
cagarridoc <cg9475301@gmail.com>
2023-10-24 00:27:42 -0300

    hola
    dsgds

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['acceso']);
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    canActivate:[AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }}
 ];
 
 @NgModule({
