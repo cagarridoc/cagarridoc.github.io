dd23333d3ea285b6a89501ce8fe2aff6eb265b07
cagarridoc <cg9475301@gmail.com>
2023-10-24 23:44:12 -0300

    fdgfgd
    fdgfdg

--- src/app/registrar/registrar.page.ts
+++ src/app/registrar/registrar.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-registrar',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar.page.scss'],
 })
 export class RegistrarPage implements OnInit {
+  data: Observable<any>;
 
-  constructor() { }
+  Nombre: String;
+  Asignatura : String;
+  Fecha: Date;
+  Asistencia : boolean = true;
+
+  constructor(private firestore: AngularFirestore, private navCtrl: NavController) { }
 
   ngOnInit() {
   }
+
+  AgregarDatos() {
+    const data = { nombre: this.Nombre, asignatura:this.Asignatura , fecha:this.Fecha , asistencia: this.Asistencia};
+    this.firestore.collection('Asistencia').add(data)
+      .then((docRef) => {
+        console.log('Documento agregado con ID: ', docRef.id);
+        this.navCtrl.back();
+      })
+      .catch((error) => {});
+}
 
 }
